b19a39e36abf2be7430431ccfeab0fcaa1d55fd6
dominicmondia <dominic.john.mondia@eee.upd.edu.ph>
2024-01-09 01:45:44 +0800

    base version

--- freeflow/settings.py
+++ freeflow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_1y=$l*pni)o6k4vs)i#c%lfwl6kjsx^x(din$epg2kx!wh6hg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,9 +116,19 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), 'mathtrainer/static',)
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Add this line if not present
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    'mathtrainer/static',
+]
+
 
 django_heroku.settings(locals())
